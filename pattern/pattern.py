900c3d6179b7c06e89dd24f81bf91d0673a319f3
S672672 <bsmith672@rku.ac.in>
2024-08-07 23:40:50 +0530

    initial commit

---
+++ pattern/pattern.py
@@ -0,0 +1,31 @@
+# *****
+# *****
+# *****
+# *****
+# *****
+# def function(n):
+#     for i in range(n):
+#             print('*'*n)
+# function(6)
+
+# *
+# **
+# ***
+# ****
+# *****
+# def function(n):
+#     for i in range(n+1):
+#         print('*'*i)
+# function(5)
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+# def function(n):
+#     for i in range(1,n+1):
+#         for j in range(1,i+1):
+#            print(j,end='');
+#         print()
+# function(5)
